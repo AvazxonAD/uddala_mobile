ec67a6ead5ac03387aa1a5c4bea4dfaaf65ac92e
AvazxonAD <asqarovavazxon77@gmail.com>
2026-04-25 15:29:28 +0500

    mobile

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://192.168.137.1:8000',
+    defaultValue: 'http://77.42.66.150:8000',
   );
 
   static const String appLang = 'uz';
